f1588e13b57758a0433e97ff6fa161838c677902
Eduardo Diaz <eduardo.diaz.268@gmail.com>
2021-04-21 15:53:04 -0500

    feat: create styles scss

---
+++ src/components/ui/objects/Container.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="tag" class="container">
+    <slot />
+  </component>
+</template>
+
+<script>
+  export default {
+    props: {
+      tag: {
+        type: String,
+        default: 'div',
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @use "@/assets/scss/settings/_variables.scss";
+
+  .container {
+    max-width: variables.$max-width;
+    margin: 0 auto;
+    padding: variables.$spacing variables.$spacing-s;
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import '@/assets/scss/index.scss';
+
 import { createApp } from 'vue';
 
-import App from './App.vue';
+import App from '@/App.vue';
 
 createApp(App).mount('#app');
