3812c3864cc01757fa68b27a703beea275b6255a
Eduardo Diaz <eduardo.diaz.268@gmail.com>
2021-04-30 04:26:28 -0500

    fix: format dayjs, add convertToF

--- src/utils/formats.ts
+++ src/utils/formats.ts
@@ -1,13 +1,22 @@
 import dayjs from 'dayjs';
 
-const round = (num?: number, dec = 2): number | string => {
-  if (!num) return '';
+const convertToF = (
+  value: number,
+  isConvert: boolean,
+  dec?: number
+): number => {
+  if (isConvert) return round(value, dec);
+  return round((value * 9) / 5 + 32, dec);
+};
+
+const round = (num?: number, dec = 1): number => {
+  if (!num) return 0;
   return Number(num?.toFixed(dec));
 };
 
 const fmDate = (date?: Date): string => {
   if (!date) return '';
-  return dayjs(date).format('ddd, d MMM');
+  return dayjs(date).format('ddd, D MMM');
 };
 
-export { fmDate, round };
+export { convertToF, fmDate, round };
