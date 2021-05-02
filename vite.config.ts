75dd86de6b68e5455bd1a1a810a472406130310b
Eduardo Diaz <eduardo.diaz.268@gmail.com>
2021-05-02 09:49:08 -0500

    feat: config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import viteSvgIcons from 'vite-plugin-svg-icons';
 
 export default defineConfig({
@@ -8,6 +9,46 @@ export default defineConfig({
     viteSvgIcons({
       iconDirs: [`${process.cwd()}/src/assets/svg`],
       symbolId: 'icon-[dir]-[name]',
+    }),
+    VitePWA({
+      // registerType: 'autoUpdate',
+      mode: 'development',
+      strategies: 'generateSW',
+      manifest: {
+        name: 'Weather APP',
+        short_name: 'weather-app',
+        description: 'Basic app to view meteorological data by location',
+        theme_color: '#0f0d1c',
+        background_color: '#0f0d1c',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        cacheId: 'weather-app',
+      },
     }),
   ],
   resolve: {
